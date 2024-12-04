9ee5a44056bdf39483b4c27346076980210ad609
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2024-12-04 18:00:46 +0700

    Update share.js

--- js/share.js
+++ js/share.js
@@ -2,6 +2,6 @@ function goToShareEmail() {
     window.location.href = 'shareEmail.html';
 }
 function shareOnFacebook() {
-    const shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=https://ititiu21330.github.io/TestingWeb/';
+    const shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=https://ititiu21330.github.io/TestingWeb/score.html';
     window.open(shareUrl, '_blank', 'width=600,height=400');
 }
