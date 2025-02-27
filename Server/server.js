daee7bddf966088ccd748d983da7b506c7ba190d
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2025-02-27 15:01:02 +0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -4,12 +4,29 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'css')));
-app.use(express.static(path.join(__dirname, 'images')));
-app.use(express.static(path.join(__dirname, 'js')));
+app.use(express.static(path.join(__dirname, '../css')));
+app.use(express.static(path.join(__dirname, '../images')));
+app.use(express.static(path.join(__dirname, '../js')));
 
+// Routes for each HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.get('/question', (req, res) => {
+  res.sendFile(path.join(__dirname, '../question.html'));
+});
+
+app.get('/share', (req, res) => {
+  res.sendFile(path.join(__dirname, '../share.html'));
+});
+
+app.get('/shareFacebook', (req, res) => {
+  res.sendFile(path.join(__dirname, '../shareFacebook.html'));
+});
+
+app.get('/score', (req, res) => {
+  res.sendFile(path.join(__dirname, '../score.html'));
 });
 
 app.listen(port, () => {
