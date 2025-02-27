40fda2dd4ae564c5aeb9ed395a04f0534fa7608f
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2025-02-27 13:32:56 +0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public', 'css')));
-app.use(express.static(path.join(__dirname, 'public', 'images')));
-app.use(express.static(path.join(__dirname, 'public', 'js')));
+app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'js')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'));
