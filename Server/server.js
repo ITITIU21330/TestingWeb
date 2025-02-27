205a107ce24ecffa515685db1345ac1f899d17ec
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2025-02-27 12:27:49 +0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public', 'css')));
+app.use(express.static(path.join(__dirname, 'public', 'images')));
+app.use(express.static(path.join(__dirname, 'public', 'js')));
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
