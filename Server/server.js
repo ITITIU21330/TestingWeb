9c219f4f4d413733f8b110e56f1c830ceb755167
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2025-02-27 15:45:13 +0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -4,35 +4,33 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Phục vụ các tệp tĩnh (CSS, hình ảnh, JS)
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'images')));
 app.use(express.static(path.join(__dirname, 'js')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes cho các file HTML
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'public' ,'index.html'));
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
 app.get('/question', (req, res) => {
-  res.sendFile(path.join(__dirname,'public' ,'question.html'));
+  res.sendFile(path.join(__dirname, 'question.html'));
 });
 
 app.get('/share', (req, res) => {
-  res.sendFile(path.join(__dirname,'public' ,'share.html'));
+  res.sendFile(path.join(__dirname, 'share.html'));
 });
 
 app.get('/shareFacebook', (req, res) => {
-  res.sendFile(path.join(__dirname,'public', 'shareFacebook.html'));
+  res.sendFile(path.join(__dirname,'shareFacebook.html'));
 });
 
 app.get('/score', (req, res) => {
-  res.sendFile(path.join(__dirname,'public', 'score.html'));
+  res.sendFile(path.join(__dirname,'score.html'));
 });
 
 app.get('/instruct', (req, res) => {
-  res.sendFile(path.join(__dirname, 'instruct.html'));
+  res.sendFile(path.join(__dirname,'instruct.html'));
 });
 
 // Khởi động server
