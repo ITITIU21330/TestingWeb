045e1322cdda784a1171136b6a0d4e9ba09e9d78
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2025-02-27 16:02:38 +0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -4,36 +4,38 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files for CSS, images, and JS
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'images')));
 app.use(express.static(path.join(__dirname, 'js')));
 
-// Routes cho các file HTML
+// Routes for HTML files
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html')); // Assuming index.html is in the root folder
 });
 
 app.get('/question', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'question.html'));
+  res.sendFile(path.join(__dirname, 'question.html')); // Assuming question.html is in the root folder
 });
 
 app.get('/share', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'share.html'));
+  res.sendFile(path.join(__dirname, 'share.html')); // Assuming share.html is in the root folder
 });
 
 app.get('/shareFacebook', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'shareFacebook.html'));
+  res.sendFile(path.join(__dirname, 'shareFacebook.html')); // Assuming shareFacebook.html is in the root folder
 });
 
 app.get('/score', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'score.html'));
+  res.sendFile(path.join(__dirname, 'score.html')); // Assuming score.html is in the root folder
 });
 
 app.get('/instruct', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'instruct.html'));
+  res.sendFile(path.join(__dirname, 'instruct.html')); // Assuming instruct.html is in the root folder
 });
 
-// Khởi động server
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
