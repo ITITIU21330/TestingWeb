018fece9c15de2b2685c7fb8d658f1aaf357c728
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2025-03-04 13:44:54 +0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.set('view engine', 'ejs'); 
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
