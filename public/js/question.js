69a5a6e10a0dc008a3ab2f585bbc1e69f39c24bc
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2025-03-04 13:49:23 +0700

    Update question.js

--- public/js/question.js
+++ public/js/question.js
@@ -67,7 +67,7 @@ function handleOptionClick(event) {
         updateQuestion();
     } else {
         localStorage.setItem("score", totalScore.toFixed(1));
-        window.location.href = "score.html";
+        window.location.href = "score.ejs";
     }
 }
 
@@ -84,7 +84,7 @@ function handleNextClick() {
         updateQuestion();
     } else {
         localStorage.setItem("score", totalScore.toFixed(1));
-        window.location.href = "score.html";
+        window.location.href = "score.ejs";
     }
 }
 
