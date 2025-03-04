c2b4d64bb14aa20438efb87af2ead654d17a5130
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2025-03-04 14:55:56 +0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -9,15 +9,6 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-app.get('/question', (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/question.html"));
-});
-
-
 app.get('/score', (req, res) => {
   let score = parseFloat(req.query.score) || 0;
   let imageUrl = `https://TestingWeb.onrender.com/images/score-${score}.jpg`; 
@@ -25,6 +16,13 @@ app.get('/score', (req, res) => {
   res.render('score', { score, imageUrl, pageUrl });
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get('/question', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/question.html"));
+});
 
 app.get('/share', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/share.html"));
@@ -48,6 +46,10 @@ app.get('/shareEmail', (req, res) => {
 
 app.get('/copyLink', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/copyLink.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).send('Page not found');
 });
 
 app.listen(port, () => {
