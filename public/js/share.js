c18a465584e66f6c7d3138f41eaaf202712e9f8d
Đức Tiến <118947902+ITITIU21330@users.noreply.github.com>
2025-03-05 12:59:05 +0700

    Update share.js

--- public/js/share.js
+++ public/js/share.js
@@ -2,11 +2,12 @@ function goToShareEmail() {
     window.location.href = 'shareEmail.html';
 }
 function shareOnFacebook() {
-    const shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=https://ititiu21330.github.io/TestingWeb/score.html';
+        const score = <%= score %>;
+        const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=https://testingweb-iyxq.onrender.com/score?score=${score}`;
     window.open(shareUrl, '_blank', 'width=600,height=400');
 }
 function copyResultLink() {
-    const resultLink = "https://ititiu21330.github.io/TestingWeb/score.html";
+    const resultLink = "https://testingweb-iyxq.onrender.com/score?score=${score}";
     navigator.clipboard.writeText(resultLink).then(() => {
         alert("Đường dẫn đã được sao chép thành công: " + resultLink);
     }).catch(err => {
